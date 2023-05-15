3cf42aac3355e77b68cba58e359c8f6303d18c21
Rohan saini <rohansaini913@gamil.com>
2023-05-15 16:38:30 +0530

    Initial Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly rwOnly=new RWOnly();
+        // this gives error "name has private access in com.driver.RWOnly"
+        //  rwOnly.name="Rohan";
+        rwOnly.setName("Rohan");
+        String name=rwOnly.getName();
+    }
 }
